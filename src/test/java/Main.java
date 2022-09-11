848ad3e9ecca137d170b23f74061dea67397a7cb
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-09-12 01:03:45 +0300

    refactor test-suite work

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,18 +1,20 @@
 import org.testng.TestNG;
-import org.testng.xml.XmlSuite;
+import org.testng.collections.Lists;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         TestNG testNG = new TestNG();
-        ArrayList<String> suitesList = new ArrayList<>();
-        suitesList.add("src/test/java/suites/system_test_suite.xml");
-        XmlSuite suite = new XmlSuite();
-        suite.setSuiteFiles(suitesList);
-        ArrayList<XmlSuite> suites = new ArrayList<>();
-        suites.add(suite);
-        testNG.setXmlSuites(suites);
+        String suiteFileName = args[0];
+
+        List<String> suites = Lists.newArrayList();
+        suites.add(suiteFileName);
+
+        // for local testing
+        // suites.add("src/test/resources/testng.xml");
+
+        testNG.setTestSuites(suites);
         testNG.run();
     }
 }
